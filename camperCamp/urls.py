f489f946e0748ebc43afa2ec3bda718829f035cc
danielsky <karma0rg4u@gmail.com>
2019-07-18 12:37:21 +0100

    Add models, views, urls and templates to blog app

--- camperCamp/urls.py
+++ camperCamp/urls.py
@@ -20,10 +20,14 @@ from django.views.static import serve
 from hello import urls as hello_urls
 from accounts import urls as accounts_urls
 from dashboard import urls as dashboard_urls
+from blog import urls as blog_urls
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('hello.urls')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^dashboard/$', include('dashboard.urls')),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import auth, messages
 
 def dashboard(request):
     return render(request, 'dashboard.html')
