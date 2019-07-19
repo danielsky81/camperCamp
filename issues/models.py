a512228add6a3f46f25aac25f209747320a21cd4
danielsky <karma0rg4u@gmail.com>
2019-07-19 23:43:28 +0100

    Add views, models, urls and forms to issues app and add modal for non users

--- issues/models.py
+++ issues/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Issue(models.Model):
+
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=False)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    views = models.IntegerField(default=0)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
--- issues/views.py
+++ issues/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from .models import Issue
+from .forms import IssueForm
 
-def issues(request):
-    return render(request, 'issues.html')
+def get_issues(request):
+    issues = Issue.objects.filter(published_date__lte=timezone.now()
+        ).order_by('-published_date')
+    return render(request, 'issues.html', {'issues': issues})
+
+def issue_detail(request, pk):
+    issue = get_object_or_404(Issue, pk=pk)
+    issue.views += 1
+    issue.save()
+    return render(request, 'issue_detail.html', {'issue': issue})
+
+
+def create_or_edit_issue(request, pk=None):
+    issue = get_object_or_404(Issue, pk=pk) if pk else None
+    if request.method == "POST":
+        form = IssueForm(request.POST, request.FILES, instance=issue)
+        if form.is_valid():
+            issue = form.save()
+            issue.author = request.user
+            issue.save()
+            return redirect(issue_detail, issue.pk)
+    else:
+        form = IssueForm(instance=issue)
+    return render(request, 'issueform.html', {'form': form})
