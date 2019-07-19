f4eb4381568919e972187c738b609b41932a4381
danielsky <karma0rg4u@gmail.com>
2019-07-19 20:04:18 +0100

    Added Heroku and Postgres support for deployment

--- blog/migrations/0007_auto_20190718_2011.py
+++ blog/migrations/0007_auto_20190718_2011.py
@@ -17,6 +17,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='post',
             name='author',
-            field=models.ForeignKey(default='Anonymous', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
