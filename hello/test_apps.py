20fee502ea79e0c0898b52aec1f2cd18bf5a34e7
danielsky <karma0rg4u@gmail.com>
2019-08-26 21:53:18 +0100

    Add testing to hello app. Coverage at 90%

---
+++ hello/test_apps.py
@@ -0,0 +1,9 @@
+from django.apps import apps
+from django.test import TestCase
+from .apps import HelloConfig
+
+class TestHelloConfig(TestCase):
+
+    def test_app(self):
+        self.assertEqual('hello', HelloConfig.name)
+        self.assertEqual('hello', apps.get_app_config('hello').name)
